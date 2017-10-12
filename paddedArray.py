ca1d153bbec802dbfa0913622c375f72f2a07a65
Zaid Hassan <zaidhassanch@gmail.com>
2017-10-12 21:18:58 +0500

    forward run tip top

--- paddedArray.py
+++ paddedArray.py
@@ -11,6 +11,7 @@ import DnnData
 embedding_size = 100;
 slice_size = 3;
 no_of_entities = 38696;
+flipType = 0;
 
 
 def memoryUsage():
@@ -70,14 +71,20 @@ E_matrix[:,1:] = word_embeds
 print memoryUsage()
 
 
+def update_x_2(inputVar):
+	return inputVar
+
+
+
 
 tree_holder       = tf.placeholder(tf.int32,   [no_of_entities,None]);
 treeLength_holder = tf.placeholder(tf.float32, [no_of_entities,]);
 E_holder          = tf.placeholder(tf.float32, [embedding_size,67448]);
-e1_holder         = tf.placeholder(tf.int32,   [None,]);
-e2_holder         = tf.placeholder(tf.int32,   [None,]);
-relation_holder   = tf.placeholder(tf.int32,   [None,]);
-e3_holder         = tf.placeholder(tf.int32,   [None,]);
+e1_holder         = tf.placeholder(tf.int32,   [dataRows,]);
+e2_holder         = tf.placeholder(tf.int32,   [dataRows,]);
+relation_holder   = tf.placeholder(tf.int32,   [dataRows,]);
+e3_holder         = tf.placeholder(tf.int32,   [dataRows,]);
+pred = tf.placeholder(tf.bool, shape=[])
 
 W1_shape = [embedding_size, embedding_size, slice_size, data.num_relations]; # change num_relations pos
 W1 = tf.Variable(tf.ones(shape=W1_shape));
@@ -87,6 +94,10 @@ b1_shape = [data.num_relations, 1, slice_size,];
 b1 = tf.Variable(tf.ones(shape=b1_shape));
 U_shape = [data.num_relations, 1, slice_size,];
 U = tf.Variable(tf.ones(shape=U_shape));
+
+cost  = tf.Variable(0, dtype = tf.float32)
+#x2 = tf.Variable([5])
+#y = tf.cond(pred, lambda: update_x_2(x), lambda: tf.identity(x))
 
 #for i in xrange(data.num_relations):
 #lst = (data.relations == i);
@@ -101,31 +112,60 @@ collectedVectors = tf.gather(Emat, tree_holder);
 sumVecs = tf.reduce_sum(collectedVectors, axis = 1);
 entVec = tf.divide(sumVecs,treeLengths);
 
-for i in xrange(1):
+# look to eliminate
+for i in xrange(data.num_relations):
 
 	lst = tf.where(tf.equal(relation_holder, i))
 	e1 = tf.gather(e1_holder,lst);
 	e2 = tf.gather(e2_holder,lst);
+	e3 = tf.gather(e3_holder,lst);
 	entVecE1 = tf.squeeze(tf.gather(entVec,e1));
 	entVecE2 = tf.squeeze(tf.gather(entVec,e2));
+	entVecE3 = tf.squeeze(tf.gather(entVec,e3));
+	#entVecE1N = tf.Variable(tf.zeros(shape = entVecE1.get_shape()));
+
+	entVecE1Neg = tf.cond(pred, lambda: update_x_2(entVecE1), lambda: update_x_2(entVecE3))
+	entVecE2Neg = tf.cond(pred, lambda: update_x_2(entVecE3), lambda: update_x_2(entVecE2))
+	e1Neg = tf.cond(pred, lambda: update_x_2(e1), lambda: update_x_2(e3))
+	e2Neg = tf.cond(pred, lambda: update_x_2(e3), lambda: update_x_2(e2))
 
 	W1transpose = tf.transpose(W1, perm=[3,2,0, 1]);
 	# restrict yourself to special i
 	W1specificTranspose = tf.gather(W1transpose,i);
+
 	firstBi = tf.tensordot(W1specificTranspose, tf.transpose(entVecE2), axes = [[2], [0]]);
+	firstBiNeg = tf.tensordot(W1specificTranspose, tf.transpose(entVecE2Neg), axes = [[2], [0]]);
 	secondB = tf.multiply(tf.transpose(entVecE1), firstBi);
+	secondBNeg = tf.multiply(tf.transpose(entVecE1Neg), firstBiNeg);
 	finalBi = tf.reduce_sum(secondB , 1);
+	finalBiNeg = tf.reduce_sum(secondBNeg , 1);
 
 	W2specific = tf.gather(W2,i);
 	b1specific = tf.gather(b1,i);
-	concatEntVecs = tf.concat([entVecE1, entVecE2], 1);
-	simpleProd = tf.add(tf.matmul(concatEntVecs, W2specific), b1specific);
+	concatEntVecs    = tf.concat([entVecE1, entVecE2], 1);
+	concatEntNegVecs = tf.concat([entVecE1Neg, entVecE2Neg], 1);
+	simpleProd    = tf.add(tf.matmul(concatEntVecs, W2specific), b1specific);
+	simpleNegProd = tf.add(tf.matmul(concatEntNegVecs, W2specific), b1specific);
 
-	v_pos = simpleProd + tf.transpose(finalBi);
+	v_pos = simpleProd    + tf.transpose(finalBi);
+	v_neg = simpleNegProd + tf.transpose(finalBiNeg);
 	z_pos = tf.tanh(v_pos);
+	z_neg = tf.tanh(v_neg);
 
 	UtransposeSpecific = tf.transpose(tf.gather(U,i));
 	score_pos		   = tf.matmul(z_pos, UtransposeSpecific);
+	score_neg		   = tf.matmul(z_neg, UtransposeSpecific);
+
+	bias = tf.constant(1, dtype = tf.float32);
+
+	indx = tf.where(tf.greater(score_pos + bias, score_neg))
+	indxJogar = tf.gather(tf.transpose(indx), 0);
+	scorePosRel = tf.gather(score_pos, tf.transpose(indxJogar));
+	scoreNegRel = tf.gather(score_neg, tf.transpose(indxJogar));
+
+	cost = cost + tf.reduce_sum((scorePosRel + bias) - scoreNegRel);
+
+
 
 
 
@@ -149,19 +189,21 @@ with tf.Session() as session:
 	print 'before session', memoryUsage()
 
 	session.run(init);
-	entVecRet, aRet, concatRet = session.run([score_pos,e2, concatEntVecs], 
+	entVecRet, aRet, concatRet = session.run([score_neg,indx, cost], 
 		feed_dict={tree_holder: out,
 				treeLength_holder: lens, 
 				E_holder         : E_matrix,
 				e1_holder        : data.e1,
 				e2_holder        : data.e2,
-				relation_holder  : data.relations, 	
-				e3_holder        : data.e3})
+				relation_holder  : data.relations,
+				e3_holder        : data.e3,
+				pred			 : True})
 	#print r;
 	#print result
 	#print result.shape;
 	print entVecRet.shape
-	print entVecRet[0]
+	print entVecRet
 	#print entVecRet[38693]
 	print aRet
-	print concatRet.shape
+	print concatRet
+
